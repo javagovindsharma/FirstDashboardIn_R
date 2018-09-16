36cacbb372a0faacba9d192db97768a097ffc88b
Govind Sharma <37431318+javagovindsharma@users.noreply.github.com>
2018-09-16 17:14:54 +0530

    Update app.R

--- app.R
+++ app.R
@@ -32,20 +32,46 @@ ui <- dashboardPage(
       
       # Second tab content
       tabItem(tabName = "widgets",
-              h2("Widgets tab content")
+              h2("Widgets tab content"),
+              # infoBoxes with fill=FALSE
+              fluidRow(
+                # A static infoBox
+                infoBox("New Orders", 10 * 2, icon = icon("credit-card")),
+                # Dynamic infoBoxes
+                infoBoxOutput("progressBox"),
+                infoBoxOutput("approvalBox")
+              )
       ),
       
       # Second tab content govind
       tabItem(tabName = "govind",
-              h2("govind tab content")
+              h2("Welcome Govind Tab using pi Chart "),
+              fluidRow(
+                box(plotOutput("govindplot", height = 250)),
+                
+                box(
+                  title = "bar",
+                  box(plotOutput("govindplot1", height = 250))
+                )
+              )
       ),
       # Second tab content pooja
       tabItem(tabName = "pooja",
-              h2("pooja tab content")
+              h2("pooja tab content"),
+              # infoBoxes with fill=TRUE
+              fluidRow(
+                infoBox("New Orders", 10 * 2, icon = icon("credit-card"), fill = TRUE),
+                infoBoxOutput("progressBox2"),
+                infoBoxOutput("approvalBox2")
+              )
       ),
       # Second tab content  vikey
       tabItem(tabName = "vikey",
-              h2("vikey tab content")
+              h2("vikey tab content"),
+              fluidRow(
+                # Clicking this will increment the progress amount
+                box(width = 4, actionButton("count", "Increment progress"))
+              )
       ),
       # Second tab content  gopal
       tabItem(tabName = "gopal",
@@ -72,6 +98,58 @@ server <- function(input, output) {
     data <- histdata[seq_len(input$slider)]
     hist(data)
   })
+  
+  
+  output$govindplot <- renderPlot({
+    data1=read.csv("/home/tattva/Desktop/govind.csv")
+    attach(data1)
+    count <- table(gender)
+    pie(count)
+    
+  })
+  output$govindplot1 <- renderPlot({
+    
+    par(mfrow=c(1,2))
+    plot(age[gender=="male"],heght[gender=="male"],xlab = "AGE",ylab = "HEIGHT",main="h vs a chart",
+         xlim = c(0,60),ylim = c(0,100))
+    plot(age[gender=="female"],heght[gender=="female"],xlab = "AGE",ylab = "HEIGHT",main="h vs a chart",
+         xlim = c(0,60),ylim = c(0,100))       
+    
+    
+  })
+  
+  
+  
+  
+  output$progressBox <- renderInfoBox({
+    infoBox(
+      "Progress", paste0(25 + input$count, "%"), icon = icon("list"),
+      color = "purple"
+    )
+  })
+  output$approvalBox <- renderInfoBox({
+    infoBox(
+      "Approval", "80%", icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "yellow"
+    )
+  })
+  
+  # Same as above, but with fill=TRUE
+  output$progressBox2 <- renderInfoBox({
+    infoBox(
+      "Progress", paste0(25 + input$count, "%"), icon = icon("list"),
+      color = "purple", fill = TRUE
+    )
+  })
+  output$approvalBox2 <- renderInfoBox({
+    infoBox(
+      "Approval", "80%", icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "yellow", fill = TRUE
+    )
+  })
+  
+  
+  
 }
 
 shinyApp(ui, server)
